2a6293aad5a2e2be59c80c708435e4827efb2aa0
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-12 13:18:05 -0400

    added database

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,10 @@
 const {expect} = require('chai')
+const {syncAndSeed} = require('../db')
 
 const app = require('supertest')(require('../app'))
 
 describe('Routes', ()=> {
+    before(() => syncAndSeed())  // no await here, we dont want a promise
     describe('GET /', ()=> {
         it('shows information about the api', async()=> {
             const response = await app.get('/')
@@ -10,4 +12,13 @@ describe('Routes', ()=> {
             expect(response.text).to.include('The Acme API')
         })
     })
+    describe('GET /api/products', ()=> {
+        it('returns products', async()=> {
+            const response = await app.get('/api/products')
+            expect(response.status).to.equal(200)
+            expect(response.body.length).to.equal(2)
+        })
+    })
+
 })
+
