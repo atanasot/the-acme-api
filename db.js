5c6cfbca625b9ef1a01f6edcd51a7f503df9fd20
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-12 13:27:23 -0400

    add travis config

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
+// need to modify travis configuration file and add db
+
+
+
 const Sequelize = require('sequelize')
 const {STRING} = Sequelize
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db')
